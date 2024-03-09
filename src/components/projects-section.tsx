1e74cf33db1739694be92f4dedb4a3fa0e3390c2
don.lino.xd <don.lino.xd@gmail.com>
2024-03-09 14:05:23 +0800

    added live site link

--- src/components/projects-section.tsx
+++ src/components/projects-section.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import WorkAsideImages from '@/components/work-aside-images'
+import Link from 'next/link'
 import { useState } from 'react'
 
 export default function ProjectsSection() {
@@ -11,7 +12,7 @@ export default function ProjectsSection() {
             project: 'MeTime Healing',
             role: 'Web Application • UI/UX Developer',
             description: 'an online wellness-counseling platform that provides services related to mental health and personal well-being.',
-            type: 'COUNSELING SYSTEM',
+            liveUrl: 'https://www.metimehealing.com/',
             images: [
                 { src: '/images/metime_mobile.svg', alt: 'metime svg' },
                 { src: '/images/metime_ipad.svg', alt: 'metime svg' },
@@ -23,7 +24,7 @@ export default function ProjectsSection() {
             role: 'Web Ecommerce Platform • Frontend Developer',
             description:
                 'an online shop that sells custom and in-stock glass products online, including table tops, mirrors, shower doors, and shelves.',
-            type: 'E-COMMERCE',
+            liveUrl: 'https://dullesglass.com/',
             images: [
                 { src: '/images/dullesglass_mobile.svg', alt: 'dullesglass' },
                 { src: '/images/dullesglass_pcmac.svg', alt: 'dullesglass' },
@@ -34,7 +35,7 @@ export default function ProjectsSection() {
             project: 'Zeno Films',
             role: 'UI/UX Designer • Web Developer',
             description: 'web app like netflix where you can watch lots of movies and TV shows whenever you want for free.',
-            type: 'MOVIE APP',
+            liveUrl: 'https://zenofilms.vercel.app/',
             images: [
                 { src: '/images/zeno_films_mobile.svg', alt: 'zeno films' },
                 { src: '/images/zeno_films_laptop.svg', alt: 'zeno films' },
@@ -46,7 +47,7 @@ export default function ProjectsSection() {
             role: 'Frontend Developer',
             description:
                 'online restaurant that serves authentic Japanese ramen dishes, meticulously crafted with traditional techniques and premium ingredients.',
-            type: 'ONLINE RESTAURANT',
+            liveUrl: 'https://rankennoodlehouse.com/',
             images: [
                 { src: '/images/ranken_noodles_mobile.svg', alt: 'ranken noodle house' },
                 { src: '/images/ranken_noodles_ipad.svg', alt: 'ranken noodle house' },
@@ -57,7 +58,7 @@ export default function ProjectsSection() {
             project: 'Sunny Agency',
             role: 'UI/UX Designer • Web Developer',
             description: 'agency website helping brands grow rapidly by crafting compelling visuals and extending brand presence in digital spaces.',
-            type: 'LANDING PAGE',
+            liveUrl: 'https://sunnyagencyworks.vercel.app/',
             images: [
                 { src: '/images/sunny_agency_tablet.png', alt: 'sunny agency' },
                 { src: '/images/sunny_agency_mobile.png', alt: 'sunny agency' },
@@ -70,7 +71,7 @@ export default function ProjectsSection() {
         <div className='flex relative bg-gray-50 text-black py-28 lg:px-[8%] min-h-[calc((320px+140vh)*5)] w-full'>
             <div className='px-[5%] text-center lg:text-start lg:px-0 items-center w-full lg:w-1/2 flex flex-col sticky top-0 lg:top-[120px] h-[calc(100vh+70px)] lg:h-[calc(100vh-240px)] justify-center gap-11 lg:gap-0 lg:justify-between lg:items-start z-10 bg-white/50 lg:bg-none backdrop-blur-sm lg:backdrop-blur-none '>
                 {works.map(
-                    ({ description, project, role, type }, idx) =>
+                    ({ description, project, role, liveUrl }, idx) =>
                         activeIndex === idx && (
                             <>
                                 <div className='after:bg-black after:absolute relative after:[content:" "] after:h-full after:animate-fill-text after:[animation-delay:0s]'>
@@ -96,9 +97,13 @@ export default function ProjectsSection() {
                                     </div>
                                 </div>
                                 <div className='after:bg-black after:absolute relative after:[content:" "] after:h-full after:animate-fill-text after:[animation-delay:0s]'>
-                                    <span className='text-xl sm:text-2xl lg:text-3xl font-medium tracking-wide animate-opacity-text [animation-delay:0.5s] opacity-0'>
-                                        {type}
-                                    </span>
+                                    <Link
+                                        href={liveUrl}
+                                        target='_blank'
+                                        className='text-xl sm:text-2xl lg:text-3xl font-medium tracking-wide animate-opacity-text [animation-delay:0.5s] opacity-0 hover:underline underline-offset-4'
+                                    >
+                                        LIVE SITE
+                                    </Link>
                                 </div>
                             </>
                         )
